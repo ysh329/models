819b3c6f1f25ab01095f374472a54ca8496ea510
ysh329 <ysh329@users.noreply.github.com>
2023-03-16 16:40:37 +0800

    Create create_tf_model.py

---
+++ create_tf_model.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+def create_reduce_sum_model(model_path, x1_size, x2_size, axes):
+    ''' Create tensorflow model for reduce_sum op.
+    '''
+    x1 = keras.Input(shape=x1_size[1:], batch_size=x1_size[0], name='X1')
+    x2 = keras.Input(shape=x2_size[1:], batch_size=x2_size[0], name='X2')
+    reduce_sum1 = tf.math.reduce_sum(x1)
+    reduce_sum2 = tf.math.reduce_sum(x2, axis=axes, keepdims=True)
+    y = tf.add(reduce_sum1, reduce_sum2, name='Y')
+
+    model = keras.models.Model([x1, x2], y)
+    # model.summary()
+
+    # save to h5 file
+    model.save(model_path, save_format="pb")
+
+
+TEST_NAME = 'reduce_sum'
+input_shape1 = [2, 3, 4, 4, 2]
+input_shape2 = [2, 5]
+
+# Generate input data
+feed_dict = {}
+feed_dict['X1:0'] = np.random.ranf(input_shape1).astype(np.float32)
+feed_dict['X2:0'] = np.random.ranf(input_shape2).astype(np.float32)
+
+for idx, axes in enumerate([1, [-2, -1], None]):
+    model_path = TEST_NAME + '-' + str(idx) + '.h5'
+    # Create model
+    create_reduce_sum_model(
+        model_path, input_shape1, input_shape2, axes)
